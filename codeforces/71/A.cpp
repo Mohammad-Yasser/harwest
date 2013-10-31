0de9d4ab2697a80207726c20470dd31989658dff
Mohammad Yasser <mohammadyasser3@gmail.com>
2013-10-31 11:26:00 +0200

    Add solution for problem `A - Way Too Long Words` Link: https://codeforces.com/contest/71/problem/A Tags: strings, *800 Ref: https://codeforces.com/contest/71/submission/4942078

--- codeforces/71/A.cpp
+++ codeforces/71/A.cpp
@@ -5,7 +5,7 @@ int main() {
     string s ;
     int n ; cin >> n ;
     for (;n>0;n--)
-   {
+    {
         cin >> s ;
         if (s.size()>10) cout << s[0] << s.size()-2<<s[s.size()-1] ;
         else cout << s ;
