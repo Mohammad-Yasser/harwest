c05d00d3514802ff9f5de0e41cf25531cd355cd1
Mohammad Yasser <mohammadyasser3@gmail.com>
2013-11-04 21:24:00 +0200

    Add solution for problem `A - Tram` Link: https://codeforces.com/contest/116/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/116/submission/4988598

--- codeforces/116/A.cpp
+++ codeforces/116/A.cpp
@@ -1,13 +1,21 @@
+//============================================================================
+// Name        : 116A.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n ; cin >> n ; int x=0,max=0,b ; n*=2 ;
-    while (n--)
-    {
-        cin >> b ;
-        if (n%2) x-=b ; else x+=b ;
-        if (x>max) max=x ;
-    }
-    cout << max ;
+	int n ; cin >> n ; int x=0,max=0,b ; n*=2 ;
+	while (n--)
+	{
+		cin >> b ;
+		if (n%2) x-=b ; else x+=b ;
+		if (x>max) max=x ;
+	}
+	cout << max ;
 }
