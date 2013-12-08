ff2c701f1c5bbe979c342870e0651e23980e802a
Mohammad Yasser <mohammadyasser3@gmail.com>
2013-12-08 20:55:00 +0200

    Add solution for problem `A - Next Round` Link: https://codeforces.com/contest/158/problem/A Tags: *special problem, implementation, *800 Ref: https://codeforces.com/contest/158/submission/5392975

--- codeforces/158/A.cpp
+++ codeforces/158/A.cpp
@@ -21,6 +21,7 @@ int main() {
     int a[n];
     for (int i = 0; i < n; i++)
         cin >> a[i];
+
     for (int i = n - 1; i >= 0; i--) {
         if (a[i] == a[k - 1] && a[k - 1] != 0 or a[i] > 0 && a[k - 1] == 0) {
             cout << i + 1;
