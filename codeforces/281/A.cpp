b9edda7f4bdb519ddbb7ef4534b59eea32cc76b4
Mohammad Yasser <mohammadyasser3@gmail.com>
2014-01-09 15:49:00 +0200

    Add solution for problem `A - Word Capitalization` Link: https://codeforces.com/contest/281/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/281/submission/5645405

---
+++ codeforces/281/A.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int> > vii;
+typedef pair<int, int> pii;
+
+const int OO = (int) 2e9;
+const double eps = 1e-9;
+
+int main() 
+{ 
+	
+	char c;
+	cin >> c ;
+	cout << char(toupper(c)) ;
+	while (cin >> c && cout << c) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
