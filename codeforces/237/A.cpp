79eb6346fb0dca6928bd55e8f56a059e51c7969d
Mohammad Yasser <mohammadyasser3@gmail.com>
2014-01-10 11:34:00 +0200

    Add solution for problem `A - Free Cash` Link: https://codeforces.com/contest/237/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/237/submission/5650912

--- codeforces/237/A.cpp
+++ codeforces/237/A.cpp
@@ -15,22 +15,20 @@ const double eps = 1e-9;
 
 int main() 
 { 
-    int n ;
-    cin >> n ;
+	int n ;
+	cin >> n ;
 
-    map <pii,int> v ;
-    while (n--)
-    {
-        int x,y ;
-        cin >> x >> y ;
-        v[make_pair(x,y)] ++ ;
-    }
-    int mx=0 ;
-    for (map<pii,int>::iterator it=v.begin() ; it!=v.end() ; ++it)
-        if (it->second > mx) mx = it->second ;
-    cout << mx ;
-
-
+	map <pii,int> v ;
+	while (n--)
+	{
+		int x,y ;
+		cin >> x >> y ;
+		v[make_pair(x,y)] ++ ;
+	}
+	int mx=0 ;
+	for (map<pii,int>::iterator it=v.begin() ; it!=v.end() ; ++it)
+		if (it->second > mx) mx = it->second ;
+	cout << mx ;
 
 
 
@@ -83,8 +81,10 @@ int main()
 
 
 
-;
 
 
-    return 0;
+
+
+
+	return 0;
 }
