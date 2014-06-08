e2d619000a76e4e32a627e45fd196a2165a9b885
Mohammad Yasser <mohammadyasser3@gmail.com>
2014-06-08 19:44:00 +0300

    Add solution for problem `C - Valera and Tubes ` Link: https://codeforces.com/contest/441/problem/C Tags: constructive algorithms, dfs and similar, implementation, *1500 Ref: https://codeforces.com/contest/441/submission/6846171

--- codeforces/441/C.cpp
+++ codeforces/441/C.cpp
@@ -2,22 +2,32 @@
 using namespace std;
 
 int days[3003];
-int n, m, k, curri = 1, currj = 1, d = 1;
 
-bool fn() {
-    cout << curri << ' ' << currj << ' ';
-    if (currj + d <= m && currj + d >= 1)
-        currj += d;
-    else
-        d *= -1, ++curri;
-    return 1;
-}
 int main() {
+
+    int n, m, k, curri = 1, currj = 1, d = 1;
     cin >> n >> m >> k;
-    for (int i = 1; i < k && cout << "2 " && fn() && fn(); cout << '\n', ++i)
-        ;
-    n = n * m - 2 * k + 2;
-    cout << n << ' ';
-    while (n-- && fn())
-        ;
+    for (int i = 1; i < k; ++i) {
+        cout << 2 << ' ' << curri << ' ' << currj;
+        if (currj + d <= m && currj + d >= 1)
+            currj += d;
+        else
+            d *= -1, ++curri;
+        cout << ' ' << curri << ' ' << currj << '\n';
+        if (currj + d <= m && currj + d >= 1)
+            currj += d;
+        else
+            d *= -1, ++curri;
+    }
+    int rem = n * m - 2 * k + 2;
+    cout << rem << ' ';
+    while (rem--) {
+        cout << curri << ' ' << currj << ' ';
+        if (currj + d <= m && currj + d >= 1)
+            currj += d;
+        else
+            d *= -1, ++curri;
+
+    }
+
 }
