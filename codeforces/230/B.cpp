e5608367c4eabff772125a46d83d3c2bf2f31c21
Mohammad Yasser <mohammadyasser3@gmail.com>
2014-08-22 19:34:00 +0300

    Add solution for problem `B - T-primes` Link: https://codeforces.com/contest/230/problem/B Tags: binary search, implementation, math, number theory, *1300 Ref: https://codeforces.com/contest/230/submission/7553956

--- codeforces/230/B.cpp
+++ codeforces/230/B.cpp
@@ -3,29 +3,30 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    bool b[1000000];
-    fill(b, b + 1000000, 1);
-    for (int i = 3; i < 1000; i += 2)
-        if (b[i])
-            for (int j = i * i; j < 1000000; b[j] = 0, j += i)
-                ;
-    int n;
-    cin >> n;
-    long long a;
+	ios_base::sync_with_stdio(0);
+	bool b[1000000];
+	fill(b, b + 1000000, 1);
+	for (int i = 3; i < 1000; i += 2)
+		if (b[i])
+			for (int j = i * i; j < 1000000; b[j] = 0, j += i)
+				;
+	int n;
+	cin >> n;
+	long long a;
 
-    while (cin >> a) {
-        double w = sqrt(a);
-        if (a == 4)
-            goto rr;
-        if (a == 1)
-            goto r;
-        if (w == (int) w && int(w) % 2 && b[(int) w])
-            rr: cout << "YES";
-        else
-            r: cout << "NO";
-        cout << endl;
-    }
+	while (cin >> a) {
+		double w = sqrt(a);
+		cerr << w;
+		if (a == 4)
+			goto rr;
+		if (a == 1)
+			goto r;
+		if (w == (int) w && int(w) % 2 && b[(int) w])
+			rr: cout << "YES";
+		else
+			r: cout << "NO";
+		cout << endl;
+	}
 
-    return 0;
+	return 0;
 }
