7b909000763d65b3dac6da3029b2fd08e952f6cb
Mohammad Yasser <mohammadyasser3@gmail.com>
2014-09-18 12:18:00 +0300

    Add solution for problem `C - Winnie-the-Pooh and honey` Link: https://codeforces.com/contest/120/problem/C Tags: implementation, math, *1100 Ref: https://codeforces.com/contest/120/submission/7824376

--- codeforces/120/C.cpp
+++ codeforces/120/C.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int main() {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    int n, k, ans = 0;
-    cin >> n >> k;
-    while (cin >> n)
-        ans += max(n % k, n - 3 * k);
-    cout << ans;
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int n, k, ans = 0;
+	cin >> n >> k;
+	while (cin >> n) {
+		for (int i = 0; i < 3; ++i)
+			if (n - k >= 0)
+				n -= k;
+		ans += n;
+	}
+	cout << ans;
 }
