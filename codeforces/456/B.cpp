1ce4693cc4fe8d976d5136e924e6416a6b07d7c5
Mohammad Yasser <mohammadyasser3@gmail.com>
2015-04-30 16:38:00 +0200

    Add solution for problem `B - Fedya and Maths` Link: https://codeforces.com/contest/456/problem/B Tags: math, number theory, *1200 Ref: https://codeforces.com/contest/456/submission/10939361

--- codeforces/456/B.cpp
+++ codeforces/456/B.cpp
@@ -4,33 +4,34 @@ using namespace std;
 typedef long long ll;
 
 ll fastPowerMod(ll base, ll power, ll mod) {
-    if (!power) return 1;
-    ll result = fastPowerMod(base, power / 2, mod);
-    result = (result * result) % mod;
-    if (power % 2) result = (result * base) % mod;
-    return result;
+	if (!power) return 1;
+	ll result = fastPowerMod(base, power / 2, mod);
+	result = (result * result) % mod;
+	if (power % 2) result = (result * base) % mod;
+	return result;
 }
 
 ll fastPowerMod(ll base, string power, ll mod) {
-    ll now = base, result = 1;
-    for (int i = power.size() - 1; i >= 0; --i) {
-        result = (result * fastPowerMod(now, power[i] - '0', mod)) % mod;
-        now = fastPowerMod(now, 10, mod);
-    }
-    return result;
+	ll tens = 1, result = 1;
+	reverse(power.begin(), power.end());
+	for (int i = 0; i < power.size(); ++i) {
+		result = (result * fastPowerMod(fastPowerMod(base, tens, mod), power[i] - '0', mod)) % mod;
+		tens *= 10;
+	}
+	return result;
 
 }
 
 int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
-    string s;
-    cin >> s;
+	string s;
+	cin >> s;
 
-    int result = 1;
-    for (int i = 2; i < 5; ++i)
-        result = (result + fastPowerMod(i, s, 5)) % 5;
+	int result = 1;
+	for (int i = 2; i < 5; ++i)
+		result = (result + fastPowerMod(i, s, 5)) % 5;
 
-    cout << result;
+	cout << result;
 
 }
