0dd367547a60c79925777a26b4f2734fc15e15e8
Mohammad Yasser <mohammadyasser3@gmail.com>
2015-05-12 16:36:00 +0200

    Add solution for problem `A - Writing Code` Link: https://codeforces.com/contest/543/problem/A Tags: dp, *1800 Ref: https://codeforces.com/contest/543/submission/11079290

--- codeforces/543/A.cpp
+++ codeforces/543/A.cpp
@@ -5,23 +5,25 @@
 
 using namespace std;
 
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+
 int n, a[505], md;
-int memo[505][505][2];
+int memo[2][505][505];
 int B, M;
 
 int main() {
 
     ios::sync_with_stdio(false);
     cin >> n >> M >> B >> md;
-    for (int i = 0; i < n; ++i)
-        cin >> a[i];
+    FOR (i , 0 , n)
+            cin >> a[i];
     for (int b = B; b >= 0; --b)
-        memo[b][M][1] = memo[b][M][0] = 1;
+        memo[1][b][M] = memo[0][b][M] = 1;
     for (int i = n - 1; i >= 0; --i)
         for (int b = B; b >= 0; --b)
             for (int m = M - 1; m >= 0; --m)
-                memo[b][m][i % 2] = (memo[b][m][(i + 1) % 2]
-                  + ((b + a[i] > B) ? 0 : memo[b + a[i]][m + 1][i % 2])) % md;
+                memo[i % 2][b][m] = (memo[(i + 1) % 2][b][m]
+                  + ((b + a[i] > B) ? 0 : memo[i % 2][b + a[i]][m + 1])) % md;
 
     cout << memo[0][0][0];
 
