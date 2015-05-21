ed54261a9070d43d9034dafbcaa13199eb874025
Mohammad Yasser <mohammadyasser3@gmail.com>
2015-05-21 15:00:00 +0200

    Add solution for problem `E - Paths and Trees` Link: https://codeforces.com/contest/545/problem/E Tags: graphs, greedy, shortest paths, *2000 Ref: https://codeforces.com/contest/545/submission/11190133

--- codeforces/545/E.cpp
+++ codeforces/545/E.cpp
@@ -1,89 +1,88 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-inline int read() {
-    int x = 0, f = 1;
-    char ch = getchar();
-    while (ch < '0' || ch > '9') {
-        if (ch == '-') f = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9') {
-        x = 10 * x + ch - '0';
-        ch = getchar();
-    }
-    return x * f;
-}
+//struct Edge {
+//	int u, v, idx;
+//	long long w;
+//	Edge(int u, int v, long long w, int i) :
+//		u(u), v(v), w(w), idx(i) {
+//	}
+//	Edge() {
+//	}
+//	bool operator <(Edge other) const {
+//		return w < other.w;
+//	}
+//};
+//
+//Edge edges[300005];
 
 int weight[300005];
 
 struct adjListElem {
-    int v, w, idx;
-    adjListElem(int v, int w, int idx) :
-        v(v), w(w), idx(idx) {
-    }
+	int v, w, idx;
+	adjListElem(int v, int w, int idx) :
+		v(v), w(w), idx(idx) {
+	}
 };
 vector<adjListElem> adjList[300005];
 
 struct PQElem {
-    int v;
-    long long w;
-    int lastEdge;
-    PQElem(int v, long long w, int lastEdge) :
-        v(v), w(w), lastEdge(lastEdge) {
-        this->w = w;
-    }
-    bool operator <(PQElem other) const {
-        if (w == other.w) return weight[lastEdge] > weight[other.lastEdge];
-        return w > other.w;
-    }
+	int v;
+	long long w;
+	int lastEdge;
+	PQElem(int v, long long w, int lastEdge) :
+		v(v), w(w), lastEdge(lastEdge) {
+		this->w = w;
+	}
+	bool operator <(PQElem other) const {
+		if (w == other.w) return weight[lastEdge] > weight[other.lastEdge];
+		return w > other.w;
+	}
 };
 
 int lastEdge[300005];
 
 void dijkstra(int u) {
-    priority_queue<PQElem> Q;
-    Q.push(PQElem(u, 0, -1));
+	priority_queue<PQElem> Q;
+	Q.push(PQElem(u, 0, -1));
 
-    while (!Q.empty()) {
+	while (!Q.empty()) {
 
-        PQElem curr = Q.top();
-        Q.pop();
+		PQElem curr = Q.top();
+		Q.pop();
 
-        if (lastEdge[curr.v]) continue;
-        lastEdge[curr.v] = curr.lastEdge;
+		if (lastEdge[curr.v]) continue;
+		lastEdge[curr.v] = curr.lastEdge;
 
-        for (int i = 0; i < adjList[curr.v].size(); ++i)
-            if (!lastEdge[adjList[curr.v][i].v])
-              Q.push(
-                PQElem(adjList[curr.v][i].v, curr.w + adjList[curr.v][i].w, adjList[curr.v][i].idx));
-    }
+		for (int i = 0; i < adjList[curr.v].size(); ++i)
+			if (!lastEdge[adjList[curr.v][i].v])
+			  Q.push(
+			    PQElem(adjList[curr.v][i].v, curr.w + adjList[curr.v][i].w, adjList[curr.v][i].idx));
+	}
 }
 
 int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= m; ++i) {
+		int ui, vi, wi;
+		cin >> ui >> vi >> wi;
+		adjList[ui].push_back(adjListElem(vi, wi, i));
+		adjList[vi].push_back(adjListElem(ui, wi, i));
+		weight[i] = wi;
+	}
 
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	int u;
+	cin >> u;
+	dijkstra(u);
 
-    int n = read(), m = read();
+	long long ans = 0;
 
-    for (int i = 1; i <= m; ++i) {
-        int ui = read(), vi = read(), wi = read();
-        adjList[ui].push_back(adjListElem(vi, wi, i));
-        adjList[vi].push_back(adjListElem(ui, wi, i));
-        weight[i] = wi;
-    }
+	for (int i = 1; i <= n; ++i)
+		if (i != u)
+		  ans += weight[lastEdge[i]];
 
-    int u = read();
-
-    dijkstra(u);
-
-    long long ans = 0;
-
-    for (int i = 1; i <= n; ++i)
-        if (i != u)
-          ans += weight[lastEdge[i]];
-
-    cout << ans << '\n';
-    for (int i = 1; i <= n; ++i)
-        if (i != u) cout << lastEdge[i] << ' ';
+	cout << ans << endl;
+	for (int i = 1; i <= n; ++i)
+		if (i != u) cout << lastEdge[i] << ' ';
 }
