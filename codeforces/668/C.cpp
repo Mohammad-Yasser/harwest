3c25fac307779a7117edcf6e3e5050120c6a249a
Mohammad Yasser <mohammadyasser3@gmail.com>
2016-04-25 11:54:00 +0200

    Add solution for problem `C - Little Artem and Random Variable` Link: https://codeforces.com/contest/668/problem/C Tags: math, probabilities, *2400 Ref: https://codeforces.com/contest/668/submission/17506482

--- codeforces/668/C.cpp
+++ codeforces/668/C.cpp
@@ -3,16 +3,17 @@ typedef long long ll;
 using namespace std;
 
 const int N = 100005, M = 303;
+const double EPS = 1e-15;
 double mx[N], mn[N];
 double p1[N], p2[N];
 int n;
 
-inline double solve(double b, double c) {
-	return (-b + sqrt(max(1e-27, b * b - 4 * c))) / 2;
+inline double solve(double a, double b, double c) {
+	return (-b + sqrt(b * b - 4 * a * c + EPS)) / (2 * a);
 }
 
 int main() {
-	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(0) , cin.tie(0) , cout.tie(0) ;
 
 	cout << fixed << setprecision(6);
 	cin >> n;
@@ -24,10 +25,13 @@ int main() {
 	double sum1 = 0, sum2 = 0;
 
 	for (int i = 0; i < n; ++i) {
-		double b = -mx[i] - sum1 + sum1 * sum2 - mn[i] + sum2 * (1 - sum1),
+		if (!mx[i] && !mn[i])
+		  continue;
+		double a = 1,
+		  b = -mx[i] - sum1 + sum1 * sum2 - mn[i] + sum2 * (1 - sum1),
 		  c = mx[i] * (1 - sum2) - mn[i] * sum2;
 
-		p2[i] = solve(b, c);
+		p2[i] = solve(a, b, c);
 		p1[i] = (mx[i] - p2[i] * sum1) / (p2[i] + sum2);
 		sum1 += p1[i];
 		sum2 += p2[i];
