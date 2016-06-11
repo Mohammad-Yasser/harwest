dcc2e4411a4503250441dab2424aa7c5b9ef0417
Mohammad Yasser <mohammadyasser3@gmail.com>
2016-06-11 14:34:00 +0200

    Add solution for problem `D - Towers` Link: https://codeforces.com/contest/229/problem/D Tags: dp, greedy, two pointers, *2100 Ref: https://codeforces.com/contest/229/submission/18374446

--- codeforces/229/D.cpp
+++ codeforces/229/D.cpp
@@ -3,36 +3,40 @@ using namespace std;
 
 const int N = 5e3 + 1;
 
-int dp[N][N];
+int memo[N][N];
 int acc[N];
 int ht[N];
 int nxt[N][N];
 int n;
 
+int dp(int ind, int lst) {
+	if (ind == n - 1) return 0;
+	int &ret = memo[ind][lst];
+	if (ret != -1) return ret;
+	if (acc[ind + 1] - acc[lst] <= ht[ind + 1]) return ret = dp(ind + 1, ind + 1);
+	ret = 1 + dp(ind + 1, lst);
+	if (nxt[lst][ind] < n) ret = min(ret, nxt[lst][ind] - ind - 1 + dp(nxt[lst][ind], ind + 1));
+	return ret;
+}
+
 int main() {
+	memset(memo, -1, sizeof memo);
 	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 	cin >> n;
 	for (int i = 0; i < n; ++i) {
-		cin >> acc[i + 1];
-		acc[i + 1] += acc[i];
+		cin >> ht[i];
+		acc[i + 1] = acc[i] + ht[i];
 	}
 
 	for (int i = 0; i < n; ++i) {
 		int curr = i;
-		for (int j = i; j >= 0; --j) {
-			while (curr <= n && 2 * acc[i + 1] - acc[j] > acc[curr])
+		for (int j = i; j < n; ++j) {
+			while (curr <= n && 2 * acc[j + 1] - acc[i] > acc[curr])
 				++curr;
 			nxt[i][j] = curr - 1;
 		}
 	}
 
-	for (int i = n - 2; i >= 0; --i)
-		for (int j = i; j >= 0; --j) {
-			int &res = dp[i][j];
-			res = 1 + dp[i + 1][j];
-			if (nxt[i][j] < n) res = min(res, nxt[i][j] - i - 1 + dp[nxt[i][j]][i + 1]);
-		}
-
-	cout << dp[0][0] << endl;
+	cout << dp(0, 0) << endl;
 }
 
