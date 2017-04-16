888aba77047f4249abf7509a23470f471e9beff5
Mohammad Yasser <mohammadyasser3@gmail.com>
2017-04-16 18:21:00 +0200

    Add solution for problem `B - Volatile Kite` Link: https://codeforces.com/contest/800/problem/B Tags: geometry, *1800 Ref: https://codeforces.com/contest/800/submission/26426511

--- codeforces/800/B.cpp
+++ codeforces/800/B.cpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 using namespace std;
-#define popCnt(x) (__builtin_popcountll(x))
-typedef unsigned long long Long;
 
-typedef double Double;
-typedef complex<Double> Point;
+typedef long long Long;
+
+typedef complex<double> Point;
 // rotate anti-clockwise
 #define EPS 1e-9
 #define OO 1e9
@@ -15,7 +12,7 @@ typedef complex<Double> Point;
 #define vec(a,b)  ((b) - (a))
 #define polar(r,t)  ((r) * exp(Point(0, (t))))
 #define angle(v)  (atan2((v).Y, (v).X))
-#define length(v)   ((Double)hypot((v).Y, (v).X))
+#define length(v)   ((double)hypot((v).Y, (v).X))
 #define lengthSqr(v)  (dot(v, v))
 #define dot(a,b)  ((conj(a) * (b)).real())
 #define cross(a,b)  ((conj(a) * (b)).imag())
@@ -23,29 +20,23 @@ typedef complex<Double> Point;
 #define rotateabout(v,t,a)  (rotate(vec(a, v), t) + (a))
 #define reflect(p,m)  ((conj((p) / (m))) * (m))
 #define normalize(p)  ((p) / length(p))
-#define same(a,b)   (length(vec(a, b)) < EPS)
+#define same(a,b)   (lengthSqr(vec(a, b)) < EPS)
 #define mid(a,b)  (((a) + (b)) / Point(2, 0))
 #define perp(a)   (Point(-(a).Y, (a).X))
 #define collinear   PointOnLine
-enum STATE {
-  IN, OUT, BOUNDRY
-};
 
-double PointLineDist(const Point& a, const Point& b, const Point& p) {
-  // handle degenrate case: (a,b) is Point
+const int N = 3e3 + 3;
+Point points[N];
+
+double pointLineDist(const Point& a, const Point& b, const Point& p) {
+  // handle degenrate case: (a,b) is point
   return fabs(cross(vec(a,b),vec(a,p)) / length(vec(a,b)));
 }
-
-const int N = 1e3 + 5;
-
-Point points[N];
 
 int main() {
   ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #ifdef Local
   freopen("test.in", "r", stdin);
-#else
-#define endl '\n'
 #endif
 
   int n;
@@ -57,14 +48,17 @@ int main() {
     points[i] = Point(x, y);
   }
 
-  double res = 1e40;
-
-  for (int i = 0; i < n; ++i) {
-    int j = (i + 1) % n, k = (j + 1) % n;
-    res = min(res, PointLineDist(points[i], points[k], points[j]));
+  for (int i = n; i < 2 * n; ++i) {
+    points[i] = points[i % n];
   }
 
-  cout << fixed << setprecision(10) << res / 2;
+  double res = 1e15;
 
+  for (int i = 1; i <= n; ++i) {
+    res = min(res, pointLineDist(points[i - 1], points[i + 1], points[i]));
+  }
+
+  cout << fixed << setprecision(6) << res / 2;
+
+  return 0;
 }
-
