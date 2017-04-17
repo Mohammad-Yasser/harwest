03aa34da19cd8d6fdb7179c45e5652bf64c9d6cf
Mohammad Yasser <mohammadyasser3@gmail.com>
2017-04-17 21:57:00 +0200

    Add solution for problem `C - Vulnerable Kerbals` Link: https://codeforces.com/contest/800/problem/C Tags: constructive algorithms, math, number theory, *2300 Ref: https://codeforces.com/contest/800/submission/26463515

--- codeforces/800/C.cpp
+++ codeforces/800/C.cpp
@@ -1,102 +1,97 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 using namespace std;
-#define popCnt(x) (__builtin_popcountll(x))
-typedef unsigned long long Long;
 
-const int N = 200005;
-vector<int> groups[N];
+const int N = 2e5 + 5;
 
-int best_sz[N];
-int best_succ[N];
-bool ban[N];
-
-int mod;
-int totient;
-
-void build() {
-  for (int i = 1; i < mod; ++i) {
-    int gcd = __gcd(i, mod);
-    if (gcd == 1) {
-      ++totient;
-    }
-    if (!ban[i]) {
-      groups[gcd].push_back(i / gcd);
-    }
+//ax+by=gcd(a,b)
+int eGCD(int a, int b, int &x, int &y) {
+  x = 1;
+  y = 0;
+  int nx = 0, ny = 1;
+  int t, r;
+  while (b) {
+    r = a / b;
+    t = a - r * b;
+    a = b;
+    b = t;
+    t = x - r * nx;
+    x = nx;
+    nx = t;
+    t = y - r * ny;
+    y = ny;
+    ny = t;
   }
-
-  memset(best_succ, -1, sizeof best_succ);
-  for (int i = mod - 1; i >= 1; --i) {
-    for (int j = 2 * i; j < mod; j += i) {
-      if (best_sz[j] > best_sz[i]) {
-        best_sz[i] = best_sz[j];
-        best_succ[i] = j;
-      }
-    }
-    best_sz[i] += groups[i].size();
-  }
+  return a;
 }
 
-int power(int b, int p) {
-  if (p == 0) return 1;
-  int res = power(b, p / 2);
-  res = 1LL * res * res % mod;
-  if (p & 1) {
-    res = 1LL * res * b % mod;
-  }
-  return res;
+//(a*mi)%m=1
+int modInv(int a, int m) {
+  int mi, r;
+  eGCD(a, m, mi, r);
+  return (mi + m) % m;
 }
 
-int modInv(int x) {
-  return power(x, totient - 1);
-}
+bool forbidden[N];
 
-vector<int> solve() {
-  vector<int> res;
-  int last_p = 1;
-  int last_g = 1;
+vector<int> comp[N];
 
-  for (int i = 1; i != -1; i = best_succ[i]) {
-    for (int x : groups[i]) {
-      int tmp = 1LL * modInv(last_p) * x % mod;
-
-      if (last_g != i) {
-        tmp = 1LL * tmp * (i / last_g) % mod;
-        last_g = i;
-      }
-      res.push_back(tmp);
-      last_p = x;
-    }
-  }
-
-  if (!ban[0]) res.push_back(0);
-  return res;
-}
+int nxt[N];
+int max_seq[N];
 
 int main() {
   ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #ifdef Local
   freopen("test.in", "r", stdin);
-#else
-#define endl '\n'
 #endif
 
-  int n;
-  cin >> n >> mod;
+  int n, m;
+  cin >> n >> m;
+
   for (int i = 0; i < n; ++i) {
     int x;
     cin >> x;
-    ban[x] = true;
+    forbidden[x] = true;
   }
 
-  build();
-
-  auto res = solve();
-  cout << res.size() << endl;
-  for (int x : res) {
-    cout << x << " ";
+  for (int i = 1; i < m; ++i) {
+    if (forbidden[i]) continue;
+    comp[__gcd(i, m)].push_back(i);
   }
-  cout << endl;
+
+  for (int i = m - 1; i > 0; --i) {
+    if (m % i != 0) continue;
+    for (int j = 2 * i; j < m; j += i) {
+      if (m % j != 0) continue;
+      if (max_seq[j] > max_seq[nxt[i]]) {
+        nxt[i] = j;
+      }
+    }
+    max_seq[i] = max_seq[nxt[i]] + comp[i].size();
+  }
+
+  vector<int> prods;
+
+  for (int i = 1; i != 0; i = nxt[i]) {
+    for (int x : comp[i]) {
+      prods.push_back(x);
+    }
+  }
+
+  int curr_prod = 1;
+
+  cout << prods.size() + !forbidden[0] << '\n';
+
+  for (int p : prods) {
+    int x = 1LL
+      * modInv(curr_prod / __gcd(m, curr_prod), m / __gcd(m, curr_prod))
+      * (p / __gcd(m, curr_prod)) % (m / __gcd(m, curr_prod));
+    cout << x << ' ';
+    curr_prod = p;
+  }
+
+  if (!forbidden[0]) {
+    cout << 0;
+  }
+
+  return 0;
 }
-
