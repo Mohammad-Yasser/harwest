cbf5c2a1008e6d6157977f4d089ede7eb2b3bac1
Mohammad Yasser <mohammadyasser3@gmail.com>
2017-07-04 18:53:00 +0200

    Add solution for problem `B - Mister B and PR Shifts` Link: https://codeforces.com/contest/819/problem/B Tags: data structures, implementation, math, *1900 Ref: https://codeforces.com/contest/819/submission/28285625

--- codeforces/819/B.cpp
+++ codeforces/819/B.cpp
@@ -10,7 +10,7 @@ const int N = 1e6 + 6;
 
 int arr[N];
 
-int cnt_pos[2 * N];
+
 
 int main() {
   ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
@@ -22,20 +22,19 @@ int main() {
   int n;
   cin >> n;
 
-  int total_neg = 0;
+  multiset<int> pos;
+  int cnt_neg = 0;
 
   Long best_val = 0;
   int best_ind = 0;
-  int total_pos = 0;
 
   for (int i = 1; i <= n; ++i) {
     cin >> arr[i];
     int x = arr[i] - i;
     if (x > 0) {
-      ++cnt_pos[x];
-      ++total_pos;
+      pos.insert(x);
     } else {
-      ++total_neg;
+      ++cnt_neg;
     }
     best_val += abs(x);
   }
@@ -44,21 +43,23 @@ int main() {
 
   for (int i = n; i > 1; --i) {
     Long tmp = prev - (n - arr[i]) + (arr[i] - 1);
-    --total_neg;
+    --cnt_neg;
 
-    tmp += total_neg;
-    tmp -= total_pos;
+    tmp += cnt_neg;
+    tmp -= pos.size();
 
-    ++cnt_pos[(arr[i] - 1) + (n - i + 1)];
-    ++total_pos;
+    pos.insert((arr[i] - 1) + (n - i + 1));
 
-    total_neg += cnt_pos[n - i + 1];
-    total_pos -= cnt_pos[n - i + 1];
+    while (!pos.empty() && *pos.begin() == n - i + 1) {
+      ++cnt_neg;
+      pos.erase(pos.begin());
+    }
 
     if (tmp < best_val) {
       best_val = tmp;
       best_ind = n - i + 1;
     }
+
 
     prev = tmp;
   }
