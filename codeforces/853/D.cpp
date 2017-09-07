adac0fee9a9b9e5d1a08e6d52541e9a6478a0bef
Mohammad Yasser <mohammadyasser3@gmail.com>
2017-09-07 13:26:00 +0200

    Add solution for problem `D - Michael and Charging Stations` Link: https://codeforces.com/contest/853/problem/D Tags: binary search, dp, greedy, *2400 Ref: https://codeforces.com/contest/853/submission/30179508

---
+++ codeforces/853/D.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long Long;
+
+const int N = 3e5 + 5;
+const int MX = 5e8;
+const int M = MX / N;
+
+int a[N];
+
+int n;
+
+
+// Didn't prove that, and think it'll get WA.
+
+
+int memo[2][M];
+
+int main() {
+#ifdef Local
+    freopen("test.in", "r", stdin);
+#endif
+
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        a[i] /= 100;
+    }
+
+
+    for (int ind = n - 1; ind >= 0; --ind) {
+        for (int card = 0; card < M; ++card) {
+            bool b = ind & 1;
+            int &res = memo[b][card];
+            res = INT_MAX;
+            int nxt_card = min(M - 1, card + a[ind] / 10);
+            res = min(res, a[ind] + memo[!b][nxt_card]);
+            int use = min(card, a[ind]);
+            nxt_card = min(M - 1, card - use);
+            res = min(res, a[ind] - use + memo[!b][nxt_card]);
+        }
+    }
+
+    cout << memo[0][0] * 100;
+}
