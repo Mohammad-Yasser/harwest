4119f0f33e22fd2242d6169fd18f2be7db157624
Mohammad Yasser <mohammadyasser3@gmail.com>
2019-07-20 18:00:00 +0200

    Add solution for problem `C - Tiles` Link: https://codeforces.com/contest/1178/problem/C Tags: combinatorics, greedy, math, *1300 Ref: https://codeforces.com/contest/1178/submission/57394874

---
+++ codeforces/1178/C.cpp
@@ -0,0 +1,42 @@
+#ifndef Local
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#endif
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define popCnt(x) (__builtin_popcountll(x))
+typedef long long Long;
+
+const int MOD = 998244353;
+
+const int N = 1003;
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int n, m;
+  cin >> n >> m;
+
+  int res = 4;
+
+  for (int i = 1; i < n; ++i) {
+    res = 2LL * res % MOD;
+  }
+
+  for (int i = 1; i < m; ++i) {
+    res = 2LL * res % MOD;
+  }
+
+  cout << res;
+
+}
