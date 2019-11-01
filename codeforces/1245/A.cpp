e93b6178b31d79ef91d5a806f015d187ccaa2102
Mohammad Yasser <mohammadyasser3@gmail.com>
2019-11-01 16:40:00 +0200

    Add solution for problem `A - Good ol' Numbers Coloring` Link: https://codeforces.com/contest/1245/problem/A Tags: math, number theory, *1000 Ref: https://codeforces.com/contest/1245/submission/63996910

---
+++ codeforces/1245/A.cpp
@@ -0,0 +1,37 @@
+#ifndef Local
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#endif
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define popCnt(x) (__builtin_popcountll(x))
+typedef long long Long;
+
+// gp_hash_table<int, int> table;
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int a , b;
+    cin >> a >> b;
+    if (__gcd(a, b) == 1) {
+      cout << "Finite" << endl;
+    } else {
+      cout << "Infinite" << endl;
+    }
+  }
+}
