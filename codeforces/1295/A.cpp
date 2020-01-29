67702ae3d0c961fd173c65d779a17f5b74798f45
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-01-29 16:42:00 +0200

    Add solution for problem `A - Display The Number` Link: https://codeforces.com/contest/1295/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/1295/submission/69738403

---
+++ codeforces/1295/A.cpp
@@ -0,0 +1,45 @@
+#ifndef Local
+#pragma GCC optimize ("O3")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#endif
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define popCnt(x) (__builtin_popcountll(x))
+typedef long long Long;
+
+// gp_hash_table<int, int> table;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int n;
+    cin >> n;
+    string s;
+    while (n > 3) {
+      s += "1";
+      n -= 2;
+    }
+    if (n == 3) {
+      s = "7" + s;
+    } else if (n == 2) {
+      s = "1" + s;
+    }
+    cout << s << endl;
+  }
+
+}
