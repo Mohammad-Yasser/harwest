a58d90e8a8fd46c204599b65e334eb780211e79d
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-04-08 21:03:00 +0200

    Add solution for problem `C - Eugene and an array` Link: https://codeforces.com/contest/1333/problem/C Tags: binary search, data structures, implementation, two pointers, *1700 Ref: https://codeforces.com/contest/1333/submission/75922625

---
+++ codeforces/1333/C.cpp
@@ -0,0 +1,64 @@
+#ifndef Local
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target( \
+    "sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx,tune=native")
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#endif
+
+#include <bits/stdc++.h>
+
+#include <ext/numeric>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using namespace std;
+
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+typedef long long Long;
+typedef vector<int> vi;
+
+#include <bits/extc++.h> /** keep-include */
+const int RANDOM =
+    chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {  // To use most bits rather than just the lowest ones:
+  const uint64_t C = Long(4e18 * acos(0)) | 71;  // large odd number
+  Long operator()(Long x) const { return __builtin_bswap64((x ^ RANDOM) * C); }
+};
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  gp_hash_table<Long, int, chash> last;
+
+  int n;
+  cin >> n;
+  vector<Long> a(n + 1, 0);
+  for (int i = 1; i <= n; ++i) {
+    cin >> a[i];
+    a[i] += a[i - 1];
+  }
+  Long res = 0;
+  last[a[n]] = n;
+  int curr_end = n;
+  for (int i = n - 1; i >= 0; --i) {
+    auto it = last.find(a[i]);
+    if (it != last.end()) {
+      curr_end = min(curr_end, it->second - 1);
+    }
+    res += curr_end - i;
+    last[a[i]] = i;
+  }
+  cout << res << endl;
+  return 0;
+}
