7200063882d5f357f170d167b0922634cb54525a
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-04-13 09:27:00 +0200

    Add solution for problem `A - Powered Addition` Link: https://codeforces.com/contest/1338/problem/A Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/1338/submission/76461068

---
+++ codeforces/1338/A.cpp
@@ -0,0 +1,51 @@
+#ifndef Local
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target( \
+    "sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx,tune=native")
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#endif
+
+#include <bits/stdc++.h>
+
+#include <ext/numeric>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using namespace std;
+
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
+
+typedef long long Long;
+typedef vector<int> vi;
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int& x : a) {
+      cin >> x;
+    }
+    int res = 0;
+    for (int i = 0; i + 1 < n; ++i) {
+      int d = a[i] - a[i + 1];
+      if (d <= 0) continue;
+      res = max(res, __lg(d) + 1);
+      a[i + 1] = a[i];
+    }
+    cout << res << endl;
+  }
+  return 0;
+}
