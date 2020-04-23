a1af668fac4ba1f214559c9a6c6a9aff507cb6af
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-04-23 23:55:00 +0200

    Add solution for problem `C - Nastya and Unexpected Guest` Link: https://codeforces.com/contest/1340/problem/C Tags: dfs and similar, dp, graphs, shortest paths, *2400 Ref: https://codeforces.com/contest/1340/submission/77856857

--- codeforces/1340/C.cpp
+++ codeforces/1340/C.cpp
@@ -1,58 +1,101 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target( \
+    "sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx,tune=native")
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+
 #include <bits/stdc++.h>
+
+#include <ext/numeric>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
 using namespace std;
-int n, m, g, r, a[10005], dist[11111111];
 
-// Not my code, trying sth
-static char buf[200 << 20];
-static size_t last_size = 0;
-static size_t buf_nxt = sizeof buf;
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
 
-void *operator new(size_t s) {
-  last_size = s;
-  return (void *)&buf[buf_nxt -= s];
-}
+typedef long long Long;
 
-void operator delete(void *ptr) {
-  if (ptr == ((void *)&buf[buf_nxt])) {
-    buf_nxt += last_size;
+const int N = 1e4 + 5;
+const int G = 1e3 + 5;
+vector<int> islands;
+
+struct State {
+  int island;
+  int green;
+  int dist;
+
+  bool operator<(const State& other) const {
+    return to_tuple() > other.to_tuple();
   }
-}
-
-int cn(int i, int j) { return i * (g + 1) + j; }
-int main() {
-  scanf("%d%d", &n, &m);
-  for (int i = 0; i < m; i++) scanf("%d", &a[i]);
-  sort(a, a + m);
-  scanf("%d%d", &g, &r);
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j <= g; j++) dist[cn(i, j)] = 1e9;
+  tuple<int, int, int> to_tuple() const {
+    return make_tuple(dist, green, island);
   }
-  deque<pair<int, int> > s;
-  s.push_back({0, 0});
-  dist[0] = 0;
-  int ans = 1e9;
-  while (!s.empty()) {
-    auto node = s.front();
-    s.pop_front();
-    int i = node.second / (g + 1), j = node.second % (g + 1);
-    if (i == m - 1) ans = min(ans, node.first * (g + r) + j);
-    if (dist[node.second] < node.first) continue;
-    vector<pair<int, int> > v;
-    if (i && j + a[i] - a[i - 1] <= g)
-      v.push_back({cn(i - 1, j + a[i] - a[i - 1]), 0});
-    if (i != m - 1 && j + a[i + 1] - a[i] <= g)
-      v.push_back({cn(i + 1, j + a[i + 1] - a[i]), 0});
-    if (j == g) v.push_back({cn(i, 0), 1});
-    for (auto u : v) {
-      if (node.first + u.second < dist[u.first]) {
-        dist[u.first] = node.first + u.second;
-        if (u.second)
-          s.push_back({dist[u.first], u.first});
-        else
-          s.push_front({dist[u.first], u.first});
+};
+
+int g, r;
+
+int dist[N][G];
+bool vis[N][G];
+
+int dijkstra() {
+  memset(dist, 0x3f, sizeof dist);
+  State initial;
+  initial.island = 0, initial.green = g, initial.dist = 0;
+  dist[0][g] = 0;
+  priority_queue<State> pq;
+  pq.emplace(initial);
+
+  while (!pq.empty()) {
+    auto curr = pq.top();
+    pq.pop();
+    if (vis[curr.island][curr.green]) continue;
+    vis[curr.island][curr.green] = true;
+    if (curr.island + 1 == islands.size()) return curr.dist;
+    State nxt;
+    if (curr.green == 0) {
+      nxt = curr;
+      nxt.green = g;
+      nxt.dist += r;
+      if (dist[nxt.island][nxt.green] <= nxt.dist) continue;
+      dist[nxt.island][nxt.green] = nxt.dist;
+      pq.emplace(nxt);
+    } else {
+      for (nxt.island = curr.island - 1; nxt.island <= curr.island + 1;
+           nxt.island += 2) {
+        if (nxt.island < 0 || nxt.island >= islands.size()) continue;
+        nxt.green =
+            curr.green - abs(islands[curr.island] - islands[nxt.island]);
+        nxt.dist = curr.dist + abs(islands[curr.island] - islands[nxt.island]);
+        if (nxt.green < 0) continue;
+        if (dist[nxt.island][nxt.green] <= nxt.dist) continue;
+        dist[nxt.island][nxt.green] = nxt.dist;
+        pq.emplace(nxt);
       }
     }
   }
-  if (ans == 1e9) ans = -1;
-  printf("%d", ans);
+
+  return -1;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int n, m;
+  cin >> n >> m;
+  islands.resize(m);
+  for (int& x : islands) {
+    cin >> x;
+  }
+  sort(islands.begin(), islands.end());
+  cin >> g >> r;
+  cout << dijkstra() << endl;
+  return 0;
 }
