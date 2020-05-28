367bf5418327fbe602faf31d88f13cca45625d8d
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-05-28 21:14:00 +0200

    Add solution for problem `C - Mixing Water` Link: https://codeforces.com/contest/1359/problem/C Tags: binary search, math, *1700 Ref: https://codeforces.com/contest/1359/submission/81823846

--- codeforces/1359/C.cpp
+++ codeforces/1359/C.cpp
@@ -19,59 +19,7 @@ typedef long double Double;
 
 const int N = 1003;
 
-template <typename Long>
-struct Fraction {
-  Long num = 0, den = 1;
-  Fraction(Long num = 0, Long den = 1) : num(num), den(den) { normalize(); }
-  void normalize() {
-    Long g = __gcd(num, den);
-    num /= g, den /= g;
-  }
-  Fraction operator*(const Fraction& other) const {
-    Fraction res = other;
-    res.num *= num;
-    res.den *= den;
-    res.normalize();
-    return res;
-  }
-  Fraction operator/(const Fraction& other) const {
-    return (*this) * Fraction(other.den, other.num);
-  }
-  Fraction operator+(const Fraction& other) const {
-    Fraction res = other;
-    Long lcm = res.den / __gcd(res.den, den) * den;
-    res.num *= lcm / res.den;
-    res.num += lcm / den * num;
-    res.den = lcm;
-    res.normalize();
-    return res;
-  }
-  Fraction operator-(const Fraction& other) const {
-    Fraction res = other * Fraction(-1);
-    return (*this) + res;
-  }
-  bool operator==(const Fraction& other) const {
-    // Must be normalized.
-    return num == other.num && den == other.den;
-  }
-  bool operator<(const Fraction& other) const {
-    return num * other.den < den * other.num;
-  }
-  friend Fraction abs(const Fraction& x) {
-    Fraction res = x;
-    res.num = abs(res.num), res.den = abs(res.den);
-    return res;
-  }
-
-  friend ostream& operator<<(ostream& os, const Fraction& f) {
-    return os << f.num << "/" << f.den << " : " << fixed << setprecision(30)
-              << 1.0L * f.num / f.den;
-  }
-};
-
-Fraction<Long> getAvg(Long n, int h, int c) {
-  return Fraction(n / 2 * c + (n + 1) / 2 * h, n);
-}
+typedef long double Double;
 
 int main() {
   ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -85,13 +33,12 @@ int main() {
   cin >> t;
   vector<Long> candidates;
   while (t--) {
-    Long h, c;
-    Fraction<Long> tmp;
-    cin >> h >> c >> tmp.num;
+    Long h, c, tmp;
+    cin >> h >> c >> tmp;
     Long low = 0, high = round(1e7 + 1), ans = 0;
     while (low <= high) {
       Long mid = (low + high) / 2;
-      Fraction avg = getAvg(2 * mid + 1, h, c);
+      Double avg = 1.0 * (mid * c + (mid + 1) * h) / (2 * mid + 1);
       if (avg < tmp) {
         ans = mid;
         high = mid - 1;
@@ -101,15 +48,15 @@ int main() {
     }
     candidates.clear();
     candidates.emplace_back(2);
-    for (int i = -1; i <= 0; ++i) {
+    for (int i = -1; i <= 1; ++i) {
       if (2 * (ans + i) + 1 > 0) candidates.emplace_back(2 * (ans + i) + 1);
     }
     sort(all(candidates));
-    Fraction best = Fraction(LLONG_MAX);
+    Double best = 1e35;
     Long res = 1;
     for (Long n : candidates) {
-      Fraction avg = getAvg(n, h, c);
-      Fraction diff = abs(avg - tmp);
+      Double avg = Double(1.0) * (n / 2 * c + (n + 1) / 2 * h) / n;
+      Double diff = abs(avg - tmp);
       if (diff < best) {
         best = diff;
         res = n;
