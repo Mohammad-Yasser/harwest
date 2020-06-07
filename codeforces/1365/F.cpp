29f6c0c4f01d2750c91263fe815835b6dcc94db5
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-06-07 20:27:00 +0200

    Add solution for problem `F - Swaps Again` Link: https://codeforces.com/contest/1365/problem/F Tags: constructive algorithms, implementation, sortings, *2100 Ref: https://codeforces.com/contest/1365/submission/82877572

---
+++ codeforces/1365/F.cpp
@@ -0,0 +1,64 @@
+#ifndef Local
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx")
+#endif
+
+#include <bits/stdc++.h>
+
+#include <ext/numeric>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+using namespace std;
+
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
+typedef long long Long;
+typedef double Double;
+
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (auto& x : v) {
+    is >> x;
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, vector<T>& v) {
+  for (auto& x : v) {
+    os << x << " ";
+  }
+  return os;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n);
+
+    cin >> a >> b;
+
+    multiset<pair<int, int>> ms_a, ms_b;
+    for (int i = 0; i < n; ++i) {
+      int j = n - 1 - i;
+      ms_a.emplace(a[i], a[j]);
+      ms_b.emplace(b[i], b[j]);
+    }
+
+    cout << (ms_a == ms_b ? "Yes" : "No") << endl;
+  }
+  return 0;
+}
