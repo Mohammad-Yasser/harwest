7a94b75722ea31a0c5afc30a24e5d60b3ea0f3ec
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-06-11 16:48:00 +0200

    Add solution for problem `A - Shovels and Swords` Link: https://codeforces.com/contest/1366/problem/A Tags: binary search, greedy, math, *1100 Ref: https://codeforces.com/contest/1366/submission/83397012

---
+++ codeforces/1366/A.cpp
@@ -0,0 +1,67 @@
+#ifndef Local
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx")
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
+typedef long long Long;
+typedef long double Double;
+
+template <class U, class V>
+istream& operator>>(istream& is, pair<U, V>& p) {
+  is >> p.first >> p.second;
+  return is;
+}
+
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (auto& x : v) {
+    is >> x;
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, vector<T>& v) {
+  for (auto& x : v) {
+    os << x << " ";
+  }
+  return os;
+}
+
+const int N = 1e7 + 5;
+Long memo[N];
+const Long OO = 1e17;
+int succ, fail;
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int t;
+  cin >> t;
+
+  while (t--) {
+    int a, b;
+    cin >> a >> b;
+    if (a > b) swap(a, b);
+    int res = 0;
+    int tmp = min(a, b - a);
+    res += tmp;
+    a -= tmp;
+    b -= 2 * tmp;
+    if (a == b) res += (a + b) / 3;
+    cout << res << endl;
+  }
+
+  return 0;
+}
