276b3e1e968bd5ea1fb0fc13652010ae50313879
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-07-04 17:08:00 +0200

    Add solution for problem `C - Element Extermination` Link: https://codeforces.com/contest/1375/problem/C Tags: constructive algorithms, data structures, greedy, *1400 Ref: https://codeforces.com/contest/1375/submission/85945654

---
+++ codeforces/1375/C.cpp
@@ -0,0 +1,62 @@
+#ifndef Local
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx")
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
+#define rall(v) rbegin(v), rend(v)
+typedef long long Long;
+typedef double Double;
+
+template <class U, class V>
+istream& operator>>(istream& is, pair<U, V>& p) {
+  is >> p.first >> p.second;
+  return is;
+}
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (auto& x : v) {
+    is >> x;
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, vector<T>& v) {
+  for (auto& x : v) {
+    os << x << " ";
+  }
+  return os;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int t;
+  cin >> t;
+  vector<int> v;
+  while (t--) {
+    int n;
+    cin >> n;
+    v.resize(n);
+    cin >> v;
+    if (v[0] < v.back()) {
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
+  }
+
+  return 0;
+}
