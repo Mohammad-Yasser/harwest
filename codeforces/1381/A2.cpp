04771969e89eeae60a02a2a3a9f748e1e04a54ea
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-07-21 16:49:00 +0200

    Add solution for problem `A2 - Prefix Flip (Hard Version)` Link: https://codeforces.com/contest/1381/problem/A2 Tags: constructive algorithms, data structures, implementation, strings, two pointers, *1700 Ref: https://codeforces.com/contest/1381/submission/87536984

---
+++ codeforces/1381/A2.cpp
@@ -0,0 +1,73 @@
+#ifndef Local
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx")
+#endif
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
+#define rall(v) rbegin(v), rend(v)
+typedef long long Long;
+typedef double Double;
+
+template <class U, class V>
+istream& operator>>(istream& is, pair<U, V>& p) {
+  is >> p.first >> p.second;
+  return is;
+}
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (auto& x : v) {
+    is >> x;
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, vector<T>& v) {
+  for (auto& x : v) {
+    os << x << " ";
+  }
+  return os;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int t;
+  cin >> t;
+
+  string a, b;
+
+  vector<int> res;
+  while (t--) {
+    int n;
+    cin >> n;
+    cin >> a >> b;
+    res.clear();
+    char curr_a = a[0];
+    for (int i = 1; i < n; ++i) {
+      if (a[i] != curr_a) {
+        res.emplace_back(i);
+        curr_a ^= 1;
+      }
+    }
+    for (int i = n - 1; i >= 0; --i) {
+      if (b[i] != curr_a) {
+        res.emplace_back(i + 1);
+        curr_a ^= 1;
+      }
+    }
+    cout << sz(res) << endl;
+    cout << res << endl;
+  }
+  return 0;
+}
