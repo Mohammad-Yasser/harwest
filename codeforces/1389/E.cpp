9b7f4850eb1a2f0f80945ade42bab630223d0a03
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-07-30 07:12:00 +0200

    Add solution for problem `E - Calendar Ambiguity` Link: https://codeforces.com/contest/1389/problem/E Tags: math, number theory, *2200 Ref: https://codeforces.com/contest/1389/submission/88402844

---
+++ codeforces/1389/E.cpp
@@ -0,0 +1,64 @@
+#ifndef Local
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,sse4.2,popcnt,abm,mmx,avx")
+#endif
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
+#define rall(v) rbegin(v), rend(v)
+typedef long long Long;
+typedef double Double;
+
+template <class U, class V>
+istream& operator>>(istream& is, pair<U, V>& p) {
+  is >> p.first >> p.second;
+  return is;
+}
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (auto& x : v) {
+    is >> x;
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, vector<T>& v) {
+  for (auto& x : v) {
+    os << x << " ";
+  }
+  return os;
+}
+
+Long sumFirst(int n) { return 1LL * n * (n + 1) / 2; }
+
+int m, d, w;
+Long solve() {
+  if (d == 1) return 0;
+  int lim = min(m, d);
+  int g = w / __gcd(w, d - 1);
+  int cnt = (lim - 1) / g;
+  Long res = 1LL * cnt * lim - sumFirst(cnt) * g;
+  return res;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int t;
+  cin >> t;
+  while (t--) {
+    cin >> m >> d >> w;
+    cout << solve() << endl;
+  }
+  return 0;
+}
