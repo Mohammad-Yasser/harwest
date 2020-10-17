ff02e8aea15a3a7fea076895f913bf33bae65540
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-10-17 15:08:00 +0200

    Add solution for problem `A - Box is Pull` Link: https://codeforces.com/contest/1428/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1428/submission/95740571

---
+++ codeforces/1428/A.cpp
@@ -0,0 +1,58 @@
+#ifndef Local
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse4,sse4.2,popcnt,abm,mmx,avx")
+#endif
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
+#define rall(v) rbegin(v), rend(v)
+#define rep(i, l, r) for (int i = l; i < r; ++i)
+typedef long long Long;
+typedef double Double;
+
+template <class U, class V>
+istream& operator>>(istream& is, pair<U, V>& p) {
+  is >> p.first >> p.second;
+  return is;
+}
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (auto& x : v) {
+    is >> x;
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+  for (auto& x : v) {
+    os << x << " ";
+  }
+  return os;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+#else
+#define endl '\n'
+#endif
+
+  int t;
+  cin >> t;
+  while (t--) {
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    int dx = abs(x1 - x2);
+    int dy = abs(y1 - y2);
+    int res = dx + dy + 2 * (min(dx, dy) != 0);
+    cout << res << endl;
+  }
+
+  return 0;
+}
