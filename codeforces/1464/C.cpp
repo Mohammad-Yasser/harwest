4357a3a4161daa0c7fa7e008b3e90af01386729f
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-12-20 20:04:00 +0200

    Add solution for problem `C - Poman Numbers` Link: https://codeforces.com/contest/1464/problem/C Tags: bitmasks, constructive algorithms, greedy, math Ref: https://codeforces.com/contest/1464/submission/101907672

---
+++ codeforces/1464/C.cpp
@@ -0,0 +1,67 @@
+#ifndef Local
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("popcnt,abm,mmx,avx2")
+#endif
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define popCnt(x) (__builtin_popcountll(x))
+#define sz(x) ((int)(x.size()))
+#define all(v) begin(v), end(v)
+#define rall(v) rbegin(v), rend(v)
+#define rep(i, l, r) for (int i = l; i < r; ++i)
+typedef long long Long;
+typedef double Double;
+
+template <class U, class V>
+istream& operator>>(istream& is, pair<U, V>& p) {
+  is >> p.first >> p.second;
+  return is;
+}
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (auto& x : v) {
+    is >> x;
+  }
+  return is;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+  for (auto& x : v) {
+    os << x << " ";
+  }
+  return os;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#ifdef Local
+  freopen("test.in", "r", stdin);
+  freopen("test.out", "w", stdout);
+#else
+#define endl '\n'
+#endif
+
+  int n;
+  Long t;
+  cin >> n >> t;
+  string s;
+  cin >> s;
+
+  vector<Long> v;
+  for (char c : s) {
+    v.emplace_back(1LL << (c - 'a'));
+  }
+  t -= v.back();
+  v.pop_back();
+  t += v.back();
+  v.pop_back();
+  sort(rall(v));
+  for (Long x : v) {
+    t = abs(t) - x;
+  }
+  cout << (t == 0 ? "Yes" : "No") << endl;
+  return 0;
+}
